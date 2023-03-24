16d8e7137efd3403f2ac7e25f82cf58b54abbe39
800147 <800147@gmail.com>
2023-03-24 10:08:21 +0300

    fullscreen button

--- js/index.js
+++ js/index.js
@@ -37,7 +37,6 @@ const Card = (label, row, col) => used(
   ),
 );
 
-
 const DEFAULT_CARDS = [ '0', '1/2', '1', '2', '3', '5', '8', '13', '20', '40', '100', '?', '\u2615' ];
 const cardsParam = new URLSearchParams(window.location.search).get('cards');
 const cards = cardsParam ? cardsParam.split(';') : DEFAULT_CARDS;
@@ -63,3 +62,69 @@ for (let row = 0; row < rows; row++) {
 }
 
 root.appendChild(__('a', { className: 'AboutLink', href: './about'}, 'about'));
+
+// fullscreen
+
+const canBeFullscreen = root.requestFullscreen !== undefined ||
+  root.webkitRequestFullscreen !== undefined;
+
+let fullscreenButtonDisabled = false;
+
+const exitFullscreen = () => {
+  if (document.fullscreenElement) {
+    fullscreenButtonDisabled = true;
+    document.exitFullscreen()
+      .catch(() => { /* Noop */ })
+      .finally(() => { fullscreenButtonDisabled = false; });
+
+    return;
+  }
+
+  if (document.webkitFullscreenElement) {
+    document.webkitCancelFullScreen();
+  }
+};
+
+const toggleFullscreen = () => {
+  if (fullscreenButtonDisabled) {
+    return;
+  }
+
+  if (document.fullscreenElement || document.webkitFullscreenElement) {
+    exitFullscreen();
+
+    return;
+  }
+
+  if (root.requestFullscreen) {
+    fullscreenButtonDisabled = true;
+    root.requestFullscreen()
+      .catch(() => { /* Noop */ })
+      .finally(() => { fullscreenButtonDisabled = false; });
+
+    return;
+  }
+
+  root.webkitRequestFullscreen?.();
+};
+
+const fullscreenButton = used(
+  el => el.addEventListener('click', toggleFullscreen),
+  __('button', { className: 'FullscreenButton' }, 'fullscreen')
+);
+
+document.addEventListener('fullscreenchange', () => {
+  console.log('document.webkitFullscreenElement', document.webkitFullscreenElement);
+  if (document.fullscreenElement === root || document.webkitFullscreenElement === root) {
+    fullscreenButton.textContent = 'exit fullscreen';
+
+    return;
+  }
+
+  fullscreenButton.textContent = 'fullscreen';
+});
+
+
+if (canBeFullscreen) {
+  root.appendChild(fullscreenButton);
+}
